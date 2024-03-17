29580a78f719cf7da81ae434d3bfccb0dd8956a3
root <root@ip-172-31-19-0.eu-west-2.compute.internal>
2024-03-17 19:19:38 +0000

    asdf

--- newsletter.php
+++ newsletter.php
@@ -7,24 +7,27 @@ $dynamodb = new DynamoDbClient([
     'region' => 'eu-west-2',
     'version' => 'latest',
     'credentials' => [
-        'key' => 'KIAQ3EGPIQ6TS36KJOP',
+        'key' => 'AKIAQ3EGPIQ6TS36KJOP',
         'secret' => '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',
     ],
 ]);
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    echo "yes";
     $email = $_GET['email'];
-    echo $email;
     $params = [
         'TableName' => 'NewsletterSubscribers',
         'Item' => [
             'email' => ['S' => $email],
         ],
     ];
-    var_dump($params);
-    $result = $dynamodb->putItem($params);
+    try {
+	       $result = $dynamodb->putItem($params);
+		    echo 'Email inserted successfully: ' . $email;
+    } catch (Aws\Exception\AwsException $e) {
+echo 'Error inserting email: ' . $e->getMessage();
+}
 echo $result;
+echo "hahahah no error"; 
     if ($result['@metadata']['statusCode'] === 200) {
         echo 'Email inserted successfully: ' . $email;
     } else {
